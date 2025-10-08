eee07c5972264fd9877719a446e41f3973082383
Carlo Conte <carlo.conte@gmail.com>
2025-10-07 21:45:22 -0300

    Abstrai

--- src/usuario/dto/CriaUsuario.dto.ts
+++ src/usuario/dto/CriaUsuario.dto.ts
@@ -1,9 +1,11 @@
 import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+import { EmailEhUnico } from "../validacao/emailEhUnico.validator";
 
 export class CriaUsuarioDTO {
     @IsNotEmpty({ message: "O nome não pode ser vazio"})
     nome: string;
     @IsEmail(undefined, { message: "O email informado é inválido"})
+    @EmailEhUnico( { message: "Já existe um usuário com este email" })
     email: string;
     @MinLength(6, { message: "A senha precisa ter pelo menos 6 caracteres"})
     senha: string;
--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { UsuarioRepository } from "./usuario.repository";
 import { UsuarioController } from "./usuario.controller";
+import { EmailEhUnicoValidator } from "./validacao/emailEhUnico.validator";
 
 @Module({
     controllers: [UsuarioController],
-    providers: [UsuarioRepository]
+    providers: [UsuarioRepository, EmailEhUnicoValidator]
 })
 
 export class UsuarioModule {}
