0823b4fbb3ee7e352387693dd4c904badd7c8287
Carlo Conte <carlo.conte@gmail.com>
2025-10-09 07:13:58 -0300

    Inicia ORM

--- src/produto/produto.entity.ts
+++ src/produto/produto.entity.ts
@@ -1,3 +1,5 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
 class CaracteristicaProduto {
   nome: string;
   descricao: string;
@@ -8,11 +10,18 @@ class ImagemProduto {
   descricao: string;
 }
 
+@Entity({ name: "produtos" })
 export class ProdutoEntity {
+  @PrimaryGeneratedColumn("uuid")
   id: string;
+
+  @Column({ name: "usuario_id", length: 100, nullable: false })
   usuarioId: string;
+  @Column({ name: "usuario_id", length: 100, nullable: false })
   nome: string;
+  @Column({ name: "usuario_id", length: 100, nullable: false })
   valor: number;
+  @Column({ name: "usuario_id", length: 100, nullable: false })
   quantidade: number;
   descricao: string;
   categoria: string;
